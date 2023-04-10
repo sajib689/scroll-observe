1bb7b5659a997e6f937467e59b5a9cb034eec368
sajib689 <jibonraja11@gmail.com>
2023-04-10 11:18:40 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const terms = document.querySelector('.terms')
+const btn = document.querySelector('.btn')
+const watch = document.querySelector('.watch')
+
+function obCallback(payload) {
+    if(payload[0].intersectionRatio === 1) {
+        btn.disabled = false
+        ob.observe(terms.lastElementChild)
+    } else {
+        btn.disabled = true
+    }
+}
+
+const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
+})
+
+ob.observe(terms.lastElementChild)
